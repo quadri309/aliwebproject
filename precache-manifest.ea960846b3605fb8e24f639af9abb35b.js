8ccf4174be3bb7057a701bd29b920feb682d936a
aliquadir <aliquadri717@gmail.com>
2020-10-18 19:35:16 -0500

    Updates

---
+++ precache-manifest.ea960846b3605fb8e24f639af9abb35b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183df3372b0c00bb9df773e4a17f7b80",
+    "url": "/aliwebproject/index.html"
+  },
+  {
+    "revision": "1e1899d59bab5601fa85",
+    "url": "/aliwebproject/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "865d1882366f767bd185",
+    "url": "/aliwebproject/static/css/main.945de223.chunk.css"
+  },
+  {
+    "revision": "1e1899d59bab5601fa85",
+    "url": "/aliwebproject/static/js/2.38e4a974.chunk.js"
+  },
+  {
+    "revision": "54e8c4f782a1df2882087d61fdec9005",
+    "url": "/aliwebproject/static/js/2.38e4a974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865d1882366f767bd185",
+    "url": "/aliwebproject/static/js/main.4cca72ca.chunk.js"
+  },
+  {
+    "revision": "b7cdfb6c4f8a384c6949",
+    "url": "/aliwebproject/static/js/runtime-main.48d7afab.js"
+  },
+  {
+    "revision": "2b64a39803bd9d0c0338c52365d3e843",
+    "url": "/aliwebproject/static/media/android.2b64a398.jpg"
+  },
+  {
+    "revision": "178c4d24bffbd5031f91cb3f1d31e318",
+    "url": "/aliwebproject/static/media/degital.178c4d24.jpg"
+  },
+  {
+    "revision": "ce56e1483146b6a0481981af5adcba6a",
+    "url": "/aliwebproject/static/media/images (1).ce56e148.jpg"
+  },
+  {
+    "revision": "a0b62667bc01025ea6a06d6efcb7888e",
+    "url": "/aliwebproject/static/media/marketing.a0b62667.jpg"
+  },
+  {
+    "revision": "b9a80e1c526a20550cf124cf004e0781",
+    "url": "/aliwebproject/static/media/software.b9a80e1c.jpg"
+  }
+]);
